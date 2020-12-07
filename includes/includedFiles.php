7682c71fdcd72f34996ecabbc685cfe436581c8e
Sourav Das <iamsouravdaswade420@gmail.com>
2020-12-07 22:07:02 +0530

    some comment added

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -20,6 +20,7 @@ else
 	$urlOftheCurrentpage=$_SERVER['REQUEST_URI'];
 	echo "<script>onPage('$urlOftheCurrentpage')</script>"; //this uses ajax but now our ajax has not included the files so we have to include the files
 	exit();
+	/* when the first time page loads the else block is executing and the content in index page is loaded so we have to close the ajax not to load the content another time*/
 	
 }
 
